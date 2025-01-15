4e4ad8731b936754f08d86dcd6d42b7adc613319
Shawn <iamshawnhartline@gmail.com>
2025-01-14 22:01:36 -0600

    Sticky Header

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// const target = document.getElementsByClassName("header").offsetTop
+
+
+
+// function changeHeader(){
+//   const point = document.getElementsByClassName("header").offsetTop
+  
+//   if (target != point){
+//     document.header.style.backgroundColor = 'red';
+//     point=document.getElementsByClassName("header").offsetTop
+//   }
+// }
